850e39b55f6ecb5c3e61e6067eb0bbaa45009401
Dax Deniega <161933956+MASTER-DAX@users.noreply.github.com>
2026-03-24 11:46:41 +0800

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 # ===============================
 # CONNECT TO MONGODB
 # ===============================
-MONGO_URI = "mongodb+srv://daxdeniega16:136541ASAka@cluster0.u2nctpk.mongodb.net/smart_cottage?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://daxdeniega16:136541asaka@cluster0.u2nctpk.mongodb.net/"
 client = MongoClient(MONGO_URI)
 
 db = client["smart_cottage"]
